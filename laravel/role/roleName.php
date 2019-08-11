44ee348d4290c41221139d8199b678f7d9226805
tiranosaur <40770066+tiranosaur@users.noreply.github.com>
2019-08-11 20:40:36 +0300

    Update roleName.php

--- laravel/role/roleName.php
+++ laravel/role/roleName.php
@@ -1,3 +1,7 @@
 <?php
 
 $roleName = User::find(Auth::user()->id)->roles()->get()->pluck('name')->toArray();
+
+
+$articles = Article::on()->orderBy('id')->paginate(10)->toArray();
+$articles = $articles['data'];
