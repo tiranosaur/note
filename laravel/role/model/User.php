8bbd85371d8b4c942a150814501c2221c4c0b2a9
tiranosaur <40770066+tiranosaur@users.noreply.github.com>
2019-08-17 02:53:20 +0300

    Update User.php

--- laravel/role/model/User.php
+++ laravel/role/model/User.php
@@ -1,82 +1,55 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App;
 
 use App\Http\Model\Role;
-use App\User;
-use Illuminate\Http\Request;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 /**
- * Class UserController
- * @package App\Http\Controllers
+ * @method static find($user_id)
  */
-class UserController extends Controller
+class User extends Authenticatable
 {
+    use Notifiable;
+
     /**
+     * The attributes that are mass assignable.
      *
+     * @var array
      */
-    public function index()
-    {
-        $users = User::all();
-        return view('user.index', ['users' => $users]);
-    }
+    protected $fillable = [
+        'name', 'username', 'email', 'password',
+    ];
 
     /**
+     * The attributes that should be hidden for arrays.
      *
+     * @var array
      */
-    public function create()
-    {
-    }
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 
     /**
-     * @param Request $request
+     * The attributes that should be cast to native types.
+     *
+     * @var array
      */
-    public function store(Request $request)
-    {
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function roles(){
+        return $this->belongsToMany(Role::class);
     }
 
-    /**
-     * @param User $user
-     */
-    public function show(User $user)
-    {
-    }
-
-    /**
-     * @param User $user
-     */
-    public function edit(User $user)
-    {
-        $roles = Role::all();
+    public static function getRoles(User $user){
         $userRoles = [];
         foreach ($user->roles as $item) {
             array_push($userRoles, $item->name);
         }
-        return view('user.edit', ['user' => $user, 'roles' => $roles, 'userRoles' => $userRoles]);
-    }
-
-    /**
-     * @param Request $request
-     * @param User $user
-     */
-    public function update(Request $request, User $user)
-    {
-        $roles = Role::all();
-        $userRoles = User::getRoles($user);
-        foreach ($roles as $item){
-            try{
-                empty($request->{$item->name}) ? $user->roles()->detach($item->id) : $user->roles()->attach($item->id);
-            }catch (\Exception $e){
-                $aa = 1;
-            }
-        }
-        return $this->index();
-    }
-
-    /**
-     * @param User $user
-     */
-    public function destroy(User $user)
-    {
+        return $userRoles;
     }
 }
