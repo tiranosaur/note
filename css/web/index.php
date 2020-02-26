bca3c8e7886c06e3c1d0b7a0f976e38fca9f5fa9
tiranosaur <40770066+tiranosaur@users.noreply.github.com>
2020-02-26 22:47:21 +0200

    Add files via upload

---
+++ css/web/index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="main.css">
+    <title>Document</title>
+</head>
+<body>
+
+
+<div class="main">
+    <div class="control-group">
+        <label class="control control-checkbox">
+            First checkbox
+            <input type="checkbox" checked="checked" />
+            <div class="control_indicator"></div>
+        </label>
+        <label class="control control-checkbox">
+            Second checkbox
+            <input type="checkbox" />
+            <div class="control_indicator"></div>
+        </label>
+        <label class="control control-checkbox">
+            Third checkbox
+            <input type="checkbox" />
+            <div class="control_indicator"></div>
+        </label>
+        <label class="control control-checkbox">
+            Disabled
+            <input type="checkbox" disabled="disabled" />
+            <div class="control_indicator"></div>
+        </label>
+        <label class="control control-checkbox">
+            Disabled & checked
+            <input type="checkbox" disabled="disabled" checked="checked" />
+            <div class="control_indicator"></div>
+        </label>
+    </div>
+</div>
+
+</body>
+</html>
