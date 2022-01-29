b18ac02507228f933845f373fa9c956025f36798
tiranosaur <40770066+tiranosaur@users.noreply.github.com>
2022-01-29 12:42:48 +0200

    Update TimestampUtil.java

--- java/util/TimestampUtil.java
+++ java/util/TimestampUtil.java
@@ -4,12 +4,25 @@ import java.util.Calendar;
 /*
     Date date = new Date();
     Timestamp timestamp = new Timestamp(date.getTime());
-    timestamp = TimestampUtil.addHours(timestamp, 288);
-    System.out.println(timestamp);
+    timestamp = TimestampUtil.getInstance().addHours(timestamp, 288);
 */
 
 public class TimestampUtil {
-    public static Timestamp addHours(Timestamp date, int hours) {
+    private static TimestampUtil INSTANCE;
+    public final Logger LOGGER = LoggerFactory.getLogger(TimestampUtil.class);
+
+    private TimestampUtil() {
+    }
+
+    public static TimestampUtil getInstance() {
+        if(INSTANCE == null) {
+            INSTANCE = new TimestampUtil();
+        }
+        return INSTANCE;
+    }
+
+    public Timestamp addHours(Timestamp date, int hours) {
+        LOGGER.debug("TimestampUtil.addHours(date = [{}], hours = [{}])", date, hours);
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
         cal.add(Calendar.HOUR, hours);
