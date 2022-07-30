56ede6157345e5d609b75f351aaef84090e9afd5
tiranosaur <40770066+tiranosaur@users.noreply.github.com>
2022-07-30 20:49:02 +0300

    Add files via upload

---
+++ golang/docker/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const http_addr = ":8080"
+
+func main() {
+	msgHandler := msg("Hello from Web Server in Go")
+	fmt.Println("Server is listening..." + http_addr)
+	http.ListenAndServe(http_addr, msgHandler)
+}
+
+type msg string
+
+func (m msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(resp, m)
+}
