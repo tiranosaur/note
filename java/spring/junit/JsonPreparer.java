b93fb10b978fa368cc342449800e8eb43994b6a7
tiranosaur <40770066+tiranosaur@users.noreply.github.com>
2022-11-09 04:55:23 +0200

    Create JsonPreparer.java

--- java/spring/junit/JsonPreparer.java
+++ java/spring/junit/JsonPreparer.java
@@ -1,25 +1,58 @@
+package util;
+
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
+import java.io.IOException;
 
-public class JsonPreparer<V> {
-    public static <T> T prepare(String fileName, TypeReference<T> typeReference) {
+public class JsonPreparer {
+    public static <T> T prepare(String fileName) {
         try {
             File file = ResourceUtils.getFile("classpath:" + fileName);
             ObjectMapper objectMapper = new ObjectMapper();
             objectMapper.registerModule(new JavaTimeModule());
 
-            return objectMapper.readValue(file, typeReference);
-        } catch (Exception e) {
+            return objectMapper.readValue(file, new TypeReference<T>() {
+            });
+        } catch (JsonParseException e) {
             e.printStackTrace();
-            return null;
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+        return null;
     }
 }
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Approver {
+    @JsonProperty("pnr")
+    private Long pnr;
+    @JsonProperty("num")
+    private String number;
+    @JsonProperty("tms")
+    private Long timestamp;
+}
 
- List<User> userList  = JsonPreparer.prepare("json/users.json", new TypeReference<List<User>>() {
-        });
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private UUID id;
+    private String name;
+    private int age;
+}
+
+List<User> userList  = JsonPreparer.prepare("json/users.json", new TypeReference<List<User>>() {});
+Approver approver = JsonPreparer.prepare("json/approver.json", new TypeReference<Approver>() {});
