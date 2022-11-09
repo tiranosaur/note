4f79750710a8e9fc62a46db4dec6fdf45bcfe621
tiranosaur <40770066+tiranosaur@users.noreply.github.com>
2022-11-09 04:35:37 +0200

    Update JsonPreparer.java

--- java/spring/junit/JsonPreparer.java
+++ java/spring/junit/JsonPreparer.java
@@ -1,34 +1,21 @@
-package util;
-
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
-import java.io.IOException;
 
-public class JsonPreparer {
-    public static <T> T prepare(String fileName) {
+public class JsonPreparer<V> {
+    public static <T> T prepare(String fileName, TypeReference<T> typeReference) {
         try {
             File file = ResourceUtils.getFile("classpath:" + fileName);
             ObjectMapper objectMapper = new ObjectMapper();
             objectMapper.registerModule(new JavaTimeModule());
 
-            return objectMapper.readValue(file, new TypeReference<T>() {
-            });
-        } catch (JsonParseException e) {
+            return objectMapper.readValue(file, typeReference);
+        } catch (Exception e) {
             e.printStackTrace();
-        } catch (JsonMappingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            return null;
         }
-
-        return null;
     }
 }
