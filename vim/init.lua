bafcefeba0cda47a6a9d8adeca558d0f5135bd3e
tiranosaur <40770066+tiranosaur@users.noreply.github.com>
2023-01-20 04:06:07 +0100

    Update init.lua

--- vim/init.lua
+++ vim/init.lua
@@ -11,10 +11,29 @@ vim.cmd([[
 	set softtabstop=4
 ]]);
 
-local options = { noremap = true }
-vim.keymap.set("i", "jj", "<Esc>", options)
 
 local Plug = vim.fn['plug#']
 vim.call('plug#begin', '~/.config/nvim/plugged')
-	Plug 'nvim-telescope/telescope.nvim'
+--	Plug 'williamboman/nvim-lsp-installer'
+--	Plug 'neovim/nvim-lspconfig'
+	-- Telescope find_files theme=get_dropdown
+		Plug 'nvim-lua/plenary.nvim'
+   		Plug 'nvim-telescope/telescope.nvim'
+	--
+	Plug 'vim-airline/vim-airline'
+	Plug 'vim-airline/vim-airline-themes'
+	--
+--	Plug 'nvim-tree/nvim-web-devicons'
+--  Plug 'nvim-tree/nvim-tree.lua'
 vim.call('plug#end')
+
+
+local options = { noremap = true }
+vim.keymap.set("i", "jj", "<Esc>", options)
+
+--Telescope
+	local builtin = require('telescope.builtin')
+	vim.keymap.set('n', 'ff', builtin.find_files, {})
+	vim.keymap.set('n', 'fg', builtin.live_grep, {})
+	vim.keymap.set('n', 'fb', builtin.buffers, {})
+	vim.keymap.set('n', 'fh', builtin.help_tags, {})
