80bd06c01b9c8826f779b8b7f76479dbe31852b9
tiranosaur <40770066+tiranosaur@users.noreply.github.com>
2024-05-05 22:35:31 +0300

    Update pdo_db_connect.php

--- php/pdo_db_connect.php
+++ php/pdo_db_connect.php
@@ -1,12 +1,41 @@
 <?php
-$host = 'localhost';
-$dbname = 'aghold';
-$username = 'root';
-$password = '';
+
+$host = "mysql8";
+$dbname = "docker";
+$username = "root";
+$password = "tiger";
+$db = new stdClass();
 
 try {
-    $DB = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    echo "Connected to $dbname at $host successfully.";
-} catch (PDOException $pe) {
-    die("Could not connect to the database $dbname :" . $pe->getMessage());
+    $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $query = "SELECT * FROM users WHERE id BETWEEN 90 AND 100";
+    $stmt = $db->query($query);
+    $authors = $stmt->fetchAll();
+    foreach ($authors as $author) {
+        echo $author["email"] . "<br>";
+    }
+
+    $query = "SELECT * FROM users WHERE id = 101";
+    $stmt = $db->query($query);
+    $author = $stmt->fetchObject();
+    print_r($author ? $author : "not found");
+
+
+    $insertQuery = "INSERT INTO users (id, name, email) VALUES (:id, :name, :email)";
+    $stmt = $db->prepare($insertQuery);
+    $stmt->execute(array("id" => 1, ':name' => 'John Doe', ':email' => 'john@example.com'));
+
+    $updateQuery = "UPDATE users SET email = :email WHERE id = :id";
+    $stmt = $db->prepare($updateQuery);
+    $stmt->execute(array(':email' => 'XXX@example.com', ':id' => 1));
+
+    $deleteQuery = "DELETE FROM users WHERE id = :id";
+    $stmt = $db->prepare($deleteQuery);
+    $stmt->execute(array(':id' => 1));
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+} finally {
+    $db = null;
 }
