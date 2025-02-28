6ade994888f43b1f292571cbb416ce3edc917ff8
tiranosaur <40770066+tiranosaur@users.noreply.github.com>
2025-02-28 09:55:07 +0200

    Update one_million_row.sql

--- postgres/one_million_row.sql
+++ postgres/one_million_row.sql
@@ -5,4 +5,5 @@ create table public.test
 );
 
 
-INSERT INTO auth.test (id, name) SELECT *,md5(random()::text) FROM generate_series(1, 1000000);
+INSERT INTO auth.test (id, name) SELECT *, md5(random()::text) FROM generate_series(1, 1000000);
+INSERT INTO auth.test (id, name) SELECT val, val * 10 FROM generate_series(1, 1000000) as val;
